ac2bf8d34b1712d97166328a66ba62f6784b3e91
helinjiang <me@helinjiang.com>
2017-02-18 23:33:42 +0800

    测试对象相关

--- 01.simple/test/object.test.js
+++ 01.simple/test/object.test.js
@@ -8,4 +8,15 @@ describe('测试对象相关', function () {
             .and.not.equal('bar');
     });
 
+    it('两个对象是否相等', function () {
+        var foo = {bar: 'baz'};
+
+        // 比较两个对象是否相等要使用 deep
+        expect(foo).to.deep.equal({bar: 'baz'});
+        expect(foo).to.not.equal({bar: 'baz'});
+
+        // deep.equal === eql
+        expect(foo).to.eql({bar: 'baz'});
+    });
+
 });
