24f612947d0c3fd53542e1d1b38f90a218c0f130
helinjiang <me@helinjiang.com>
2017-02-19 08:25:06 +0800

    deep referencing

--- 01.simple/test/object.test.js
+++ 01.simple/test/object.test.js
@@ -2,11 +2,14 @@ var expect = require('chai').expect;
 
 describe('测试对象相关', function () {
 
-    it('{foo: "baz"} 拥有属性 foo，且其值为 baz 而不是 bar', function () {
+    it('{foo: "baz"} 拥有属性 foo，且其值为 baz 而不是 bar，值为string类型', function () {
+        // 注意：property 方法改变了断言返回值，返回的是这个属性对应的值，而不再是原始的对象。
         expect({foo: 'baz'}).to.have.property('foo')
+            .that.is.a('string')
             .and.equal('baz')
             .and.not.equal('bar');
 
+        // 简单判断属性和属性值的方式
         expect({foo: 'baz'}).to.have.property('foo', 'baz');
     });
 
@@ -23,6 +26,18 @@ describe('测试对象相关', function () {
         expect(deepObj).to.have.deep.property('green.tea', 'matcha');
         expect(deepObj).to.have.deep.property('teas[1]', 'matcha');
         expect(deepObj).to.have.deep.property('teas[2].tea', 'konacha');
+        expect(deepObj).to.have.property('teas')
+            .that.is.an('array')
+            .with.deep.property('[2]')
+            .that.deep.equals({ tea: 'konacha' });
+
+        var arr = [
+            ['chai', 'matcha', 'konacha'],
+            [{tea: 'chai'}, {tea: 'matcha'}, {tea: 'konacha'}]
+        ];
+
+        expect(arr).to.have.deep.property('[0][1]', 'matcha');
+        expect(arr).to.have.deep.property('[1][2].tea', 'konacha');
     });
 
     it('两个对象是否相等', function () {
