f62d7169e4536c6eae0a22a956dd49904718a6ce
helinjiang <me@helinjiang.com>
2017-02-19 08:16:57 +0800

    测试对象相关

--- 01.simple/test/object.test.js
+++ 01.simple/test/object.test.js
@@ -6,6 +6,23 @@ describe('测试对象相关', function () {
         expect({foo: 'baz'}).to.have.property('foo')
             .and.equal('baz')
             .and.not.equal('bar');
+
+        expect({foo: 'baz'}).to.have.property('foo', 'baz');
+    });
+
+    it('deep referencing 属性测试', function () {
+        var deepObj = {
+            green: {
+                tea: 'matcha'
+            },
+            teas: ['chai', 'matcha', {
+                tea: 'konacha'
+            }]
+        };
+
+        expect(deepObj).to.have.deep.property('green.tea', 'matcha');
+        expect(deepObj).to.have.deep.property('teas[1]', 'matcha');
+        expect(deepObj).to.have.deep.property('teas[2].tea', 'konacha');
     });
 
     it('两个对象是否相等', function () {
@@ -17,6 +34,19 @@ describe('测试对象相关', function () {
 
         // deep.equal === eql
         expect(foo).to.eql({bar: 'baz'});
+    });
+
+    it('{} 是一个空对象', function () {
+        var foo = {};
+
+        // object 类型
+        expect(foo).to.be.a('object');
+
+        // 不是null也不是undefined
+        expect(foo).to.exist;
+
+        // 不包含key值
+        expect(foo).to.be.empty;
     });
 
 });
